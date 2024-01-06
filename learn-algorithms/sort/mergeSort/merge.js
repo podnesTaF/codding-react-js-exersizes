e1f07948d21920f611e04249e801f797ab4185a1
podnesTaF <podnestrade@gmail.com>
2024-01-06 21:23:55 +0100

    start complex sorting

---
+++ learn-algorithms/sort/mergeSort/merge.js
@@ -0,0 +1,12 @@
+const merge = (arr1, arr2) => {
+  let length = Math.min(arr1.length, arr2.length);
+  let res = [];
+
+  for (let i = 0; i < length; i++) {
+    let j = i;
+  }
+
+  return res;
+};
+
+console.log(merge([1, 10, 50], [2, 14, 99, 100]));
